01049b5a1bf4b767978a78069c1fd1f214bfcd56
dervesp <dervesp@gmail.com>
2015-03-26 21:23:01 +0300

    Update spider.inc.php
    minor changes

--- include/spider.inc.php
+++ include/spider.inc.php
@@ -1,15 +1,44 @@
 <?php
+    function runSpider($homeUrl)
+    {
+        $linkContainer = array();
+        $linkContainer['homeUrl'] = parse_url($homeUrl);
+        $content = getFileContent($homeUrl, '');
+        saveFileContent($content, $homeUrl);
+        getLinks($content, $linkContainer);
+        $wasUnsavedLink = true;
+        while ($wasUnsavedLink)
+        {
+            $wasNewLink = false;
+            foreach ($linkContainer as $link => &$isLinkDone)
+            {
+                if ($link != 'homeUrl')
+                {
+                    if (!$isLinkDone) {
+                        $url = $linkContainer['homeUrl']['scheme'] . '://' . $linkContainer['homeUrl']['host'] . $link;
+                        $content = getFileContent($url, '');
+                        saveFileContent($content, $url);
+                        getLinks($content, $linkContainer);
+                        $isLinkDone = true;
+                        $wasUnsavedLink = true;
+                    }  
+                }
+            }
+        }
+    }
+    
     function getFileContent($url, $divId)
     {
-        set_time_limit(60);
+        set_time_limit(60); //CRUTCH
         $fileContent = file_get_contents($url);
+        //TODO: parse content for correct div
         return ($fileContent === false)? "" : $fileContent;
     }
     
     function saveFileContent($fileContent, $fileUri)
     {
-        //debug
-        echo 'saved ' . $fileUri . "\n";
+        //TODO: save content
+        echo 'saved ' . $fileUri . "\n"; //debug
     }
     
     function getLinks($fileContent, &$linkContainer)
@@ -31,35 +60,4 @@
                 }
             //}
         }
-    }
-    
-    function runSpider($homeUrl)
-    {
-        $linkContainer = array();
-        $linkContainer['homeUrl'] = parse_url($homeUrl);
-        $content = getFileContent($homeUrl, '');
-        saveFileContent($content, $homeUrl);
-        getLinks($content, $linkContainer);
-        $wasUnsavedLink = true;
-        $i = 0;
-        while (($wasUnsavedLink) && ($i<1000))
-        {
-            $wasNewLink = false;
-            foreach ($linkContainer as $link => &$isLinkDone)
-            {
-                if ($link != 'homeUrl')
-                {
-                    if (!$isLinkDone) {
-                        $url = $linkContainer['homeUrl']['scheme'] . '://' . $linkContainer['homeUrl']['host'] . $link;
-                        $content = getFileContent($url, '');
-                        saveFileContent($content, $url);
-                        getLinks($content, $linkContainer);
-                        $isLinkDone = true;
-                        $wasUnsavedLink = true;
-                        $i++;
-                    }  
-                }
-            }
-        }
-        
     }
